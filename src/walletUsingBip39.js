10b88ceed6c9030cdac20da0c582fb039fd12e2d
divakar-1221 <Divakarl@possibleworks.com>
2023-04-02 16:03:29 +0530

    derived path using index

--- src/walletUsingBip39.js
+++ src/walletUsingBip39.js
@@ -1,20 +1,24 @@
 const bip39 = require("bip39");
 const { Wallet, hdkey } = require("ethereumjs-wallet");
-// const mnemonic = bip39.generateMnemonic(128); // 12 string mnemonic phrase as 128 bits
-// console.log("mnemonic using bip39 is ",mnemonic);
 
-// got this using bip39 from console
-const mnemonicFromBip39 =
-  "fiction search sock valid task delay raccoon siege ceiling leaf crime view";
-async function createWallFromBip() {
-  const seed = await bip39.mnemonicToSeed(mnemonicFromBip39);
+async function createWallFromBip(index=0) {
+    const mnemonic = bip39.generateMnemonic(128); // 12 string mnemonic phrase as 128 bits
+  const seed = await bip39.mnemonicToSeed(mnemonic);
   // buffer to hex rep string
   console.log("Seed:", seed.toString("hex"));
   //from seed to wallet
   const hdNode = hdkey.fromMasterSeed(seed);
-  const path = `m/44'/60'/0'/0/0`;
-  const wallet = hdNode.derivePath(path).getWallet();
+  //   const path = `m/44'/60'/0'/0/0`;
+
+  //can also be done in one line using '.' operator
+  //   const derive=hdNode.derivePath(`m/44'/60'/0'`).deriveChild(0).deriveChild(0);
+  //   const wallet = derive.getWallet();
+
+  const derive=hdNode.derivePath(`m/44'/60'/0'`);
+  const child=derive.deriveChild(index);
+  const nextChild=child.deriveChild(index);
+  const wallet = nextChild.getWallet();
   console.log("privateKey: ", wallet.getPrivateKeyString());
   console.log("address: ", wallet.getAddressString());
 }
-createWallFromBip();
+createWallFromBip(1);
